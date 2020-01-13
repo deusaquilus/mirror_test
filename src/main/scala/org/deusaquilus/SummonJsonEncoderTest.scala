68797786e0cf08fe89ced27bd02ac500e76ace3a
deusaquilus <deusaquilus@gmail.com>
2020-01-12 21:06:44 -0500

    Works using summonFrom

--- src/main/scala/org/deusaquilus/SummonJsonEncoderTest.scala
+++ src/main/scala/org/deusaquilus/SummonJsonEncoderTest.scala
@@ -8,20 +8,9 @@ import JsonEncoder.{given, _}
 
 object SummonJsonEncoderTest {
 
-  inline def encodeAndMessAroundType[T](value: =>T): String = ${ encodeAndMessAroundTypeImpl('value) }
-
-  def encodeAndMessAroundTypeImpl[T](value: Expr[T])(given qctx: QuoteContext, t: Type[T]): Expr[String] = {
-    import qctx.tasty.{_, given}
-
-    val mirrorTpe = '[Mirror.Of[$t]]
-    val mirrorExpr = summonExpr(given mirrorTpe) match {
-      case Some(mirror) => mirror
-    }
-
-    '{
-      given JsonEncoder[T] = JsonEncoder.derived($mirrorExpr)
-      val encoder = summon[JsonEncoder[$t]]
-      encoder.encode($value)
+  inline def encodeAndMessAroundType[T](value: =>T): String = {
+    summonFrom {
+      case m: Mirror.ProductOf[T] => derived(m).encode(value)
     }
   }
 }
